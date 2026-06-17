effad6af5f0fa7d707b1fa9e9548f533444ea67e
emran bolouri <emransix@gmail.com>
2026-06-17 20:40:40 +0330

    feat: added utils ,routes and config app

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/emious/go-book-management-api/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+func RegisterBookStoreRoutes(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
+}
